2b7932f8287836c5f50e12eaa6d2694da94dfa85
Tuan Dung TRAN <mcafee92@gmail.com>
2018-10-21 14:41:05 +0900

    prepare backend api

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
